f575810118e17e93bafaeb168a6660d47595501e
Caio Henrique <henriquecaio898@gmail.com>
2024-09-02 11:57:34 -0300

    Desafio de fluxo dio

--- src/ParametrosIvalidosException.java
+++ src/ParametrosIvalidosException.java
@@ -1,7 +1,7 @@
 public class ParametrosIvalidosException {
     static void contar(int parametroUm, int parametroDois) throws Exception{
         if(parametroUm > parametroDois) {
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            throw new Exception("O segundo parâmetro deve ser maior que o primeiro");
         }else {
             int contagem = parametroDois - parametroUm;
             for(int i = 1; i<=contagem; i++) {
